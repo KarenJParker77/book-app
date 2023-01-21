440bc7323525888f82e3642ccce2cb6c0b9edc8a
KarenJParker77 <karen.mccrackle@gmail.com>
2023-01-21 17:50:11 +0000

    first commit

---
+++ src/fakeApi/index.js
@@ -0,0 +1,172 @@
+// define data structure
+
+//
+
+export const user = {
+  id: 1,
+  userName: "",
+  // dateJoined: 12345678910,
+  currentBook: [],
+  futureBooks: [],
+  finishedBooks: [],
+};
+
+export const allBooks = [
+  {
+    id: 1,
+    bookTitle: "Harry Potter and the Philosopher's Stone",
+    author: "JK Rowling",
+    image: "/HarryPotter.jpg",
+  },
+  {
+    id: 2,
+    bookTitle: "Winnie the Pooh",
+    author: "AA Milne",
+    read: true,
+    image: "/winnie-the-pooh.jpg",
+  },
+  {
+    id: 3,
+    bookTitle: "The Lincoln Lawyer",
+    author: "Michael Connelly",
+    read: true,
+    image: "/lincoln-lawyer.jpg",
+  },
+  {
+    id: 4,
+    bookTitle: "Verity",
+    author: "Colleen Hoover",
+    read: true,
+    image: "/verity.jpg",
+  },
+
+  {
+    id: 5,
+    bookTitle: "The Gods of Guilt",
+    author: "Michael Connelly",
+    read: true,
+    image: "./Gods-Guilt.jpg",
+  },
+
+  {
+    id: 6,
+    bookTitle: "The Ink Black Heart",
+    author: "Robert Galbraith",
+    read: true,
+    image: "/ink-black-heart.jpg",
+  },
+
+  {
+    id: 7,
+    bookTitle: "Lessons in Chemistry",
+    author: "Bonnie Garmus",
+    read: true,
+    image: "/lessons.jpg",
+  },
+
+  {
+    id: 8,
+    bookTitle: "Pride and Prejudice",
+    author: "Jane Austen",
+    image: "/pride1.webp",
+  },
+  {
+    id: 12,
+    bookTitle: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    image: "https://images.gr-assets.com/books/1388282444l/386162.jpg",
+  },
+  {
+    id: 13,
+    bookTitle: "The Giving Tree",
+    author: "Shel Silverstein",
+    image: "https://images.gr-assets.com/books/1174210942l/370493.jpg",
+  },
+  {
+    id: 14,
+    bookTitle: "Wuthering Heights",
+    author: "Emily Brontë",
+    image: "https://images.gr-assets.com/books/1388212715l/6185.jpg",
+  },
+  {
+    id: 15,
+    bookTitle: "The Da Vinci Code",
+    author: "Dan Brown",
+
+    image: "https://images.gr-assets.com/books/1303252999l/968.jpg",
+  },
+  {
+    id: 16,
+    bookTitle: "Memoirs of a Geisha",
+    author: "Arthur Golden",
+    image: "https://images.gr-assets.com/books/1388367666l/930.jpg",
+  },
+  {
+    id: 17,
+    bookTitle: "Alice's Adventures in Wonderland",
+    author: "Lewis Carroll",
+    image: "https://images.gr-assets.com/books/1327872220l/24213.jpg",
+  },
+  {
+    id: 22,
+    bookTitle: "Romeo and Juliet",
+    author: "William Shakespeare",
+    image: "https://images.gr-assets.com/books/1327872146l/18135.jpg",
+  },
+  {
+    id: 23,
+    bookTitle: "Lord of the Flies",
+    author: "William Golding",
+    image: "https://images.gr-assets.com/books/1327869409l/7624.jpg",
+  },
+  {
+    id: 24,
+    bookTitle: "The Alchemist",
+    author: "Paulo Coelho",
+    image: "https://images.gr-assets.com/books/1483412266l/865.jpg",
+  },
+  {
+    id: 25,
+    bookTitle: "Crime and Punishment",
+    author: "Fyodor Dostoyevsky",
+    image: "https://images.gr-assets.com/books/1382846449l/7144.jpg",
+  },
+  {
+    id: 26,
+    bookTitle: "Ender's Game",
+    author: "Orson Scott Card",
+
+    image: "https://images.gr-assets.com/books/1408303130l/375802.jpg",
+  },
+  {
+    id: 27,
+    bookTitle: "The Perks of Being a Wallflower",
+    author: "Stephen Chbosky",
+    image: "https://images.gr-assets.com/books/1520093244l/22628.jpg",
+  },
+  {
+    id: 28,
+    bookTitle: "City of Bones",
+    author: "Cassandra Clare",
+    image: "https://images.gr-assets.com/books/1432730315l/256683.jpg",
+  },
+  {
+    id: 29,
+    bookTitle: "The Help",
+    author: "Kathryn Stockett",
+    genre_list: "Fiction,Historical,Historical Fiction,Adult",
+    image: "https://images.gr-assets.com/books/1346100365l/4667024.jpg",
+  },
+  {
+    id: 30,
+    bookTitle: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    image: "https://images.gr-assets.com/books/1490528560l/4671.jpg",
+  },
+];
+
+// export const futureBooks = [{}];
+
+// bookId: [2], userId: 0, whenAdded: "date"
+
+// export const finishedBooks = [{ bookId: [3], whenFinished: "date" }];
